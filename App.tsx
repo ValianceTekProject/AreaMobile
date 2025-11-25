0c15193deb97d719f76ffe4fb41907df4fdac3f3
AMissteryKitty <aline.nguyen@epitech.eu>
2025-11-25 22:51:00 +0100

    feat: adding PoC of react-native-paper for components ui and axios for call apis

--- App.tsx
+++ App.tsx
@@ -5,8 +5,10 @@
  * @format
  */
 
+import axios from "axios";
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { Card, Text, Button } from 'react-native-paper';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -23,15 +25,42 @@ function App() {
   );
 }
 
+
+const baseUrl = "http://10.0.2.2:8080";
+
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+
+  const onOkButton = async () => {
+    await axios.post(`${baseUrl}/todos/add`, {
+      "Item": "Test",
+      "Completed": false
+    });
+  };
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Card>
+        <Card.Title title="Card Title" subtitle="Card Subtitle" />
+        <Card.Content>
+          <Text variant="titleLarge">Card title</Text>
+          <Text variant="bodyMedium">Card content</Text>
+        </Card.Content>
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button onPress={onOkButton}>Ok</Button>
+        </Card.Actions>
+      </Card>
+      <Card>
+        <Card.Title title="Card Title" subtitle="Card Subtitle" />
+        <Card.Content>
+          <Text variant="titleLarge">Card title</Text>
+          <Text variant="bodyMedium">Card content</Text>
+        </Card.Content>
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button onPress={onOkButton}>Ok</Button>
+        </Card.Actions>
+      </Card>
     </View>
   );
 }
