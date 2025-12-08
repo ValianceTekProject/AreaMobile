1187119afbfef6eae066b15af0c8151c871cf724
AMissteryKitty <aline.nguyen@epitech.eu>
2025-12-08 13:38:49 +0100

    feat: adding Dashboard page

---
+++ src/features/dashboard/DashboardPage.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { Card, TextInput, Text, Button, IconButton, MD3Colors } from 'react-native-paper';
+import { StyleSheet, View, useColorScheme } from 'react-native';
+import axios from "axios";
+
+export default function DashboardPage() {
+  const baseUrl = "http://10.0.2.2:8080";
+  const theme = useColorScheme();
+
+    type Todo = {
+        action: string;
+        reaction: string;
+        };
+
+    const [items, setItems] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    setItems([{ "action": "blablabla",
+                    "reaction": "test"
+                },
+                { "action": "blablabla",
+                    "reaction": "test"
+                }]);
+    }, []);
+
+  return (
+    <View style={styles.container}>
+      {items.map((item, index) => (
+        <Card key={index} style={styles.card}>
+          <Card.Content>
+            <Text variant="titleLarge">Action: {item.action}</Text>
+            <Text variant="titleLarge">Reaction: {item.reaction}</Text>
+          </Card.Content>
+          <Card.Actions>
+            <Button>Apply</Button>
+          </Card.Actions>
+        </Card>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  card: {
+    width: "90%",
+    height: "20%",
+    margin: 10
+  },
+  inputs: {
+    margin: 10
+  }
+});
